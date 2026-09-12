f7cd630d8da9aefe7e5ceda88e7ba3fdf8eedbf5
viect6o <phamvietthang2006hd@gmail.com>
2026-09-12 21:54:54 +0700

    Process data: merge via SK_ID_CURR

--- srcs/data_preparation/merge_main_table.py
+++ srcs/data_preparation/merge_main_table.py
@@ -1,64 +1,87 @@
 import pandas as pd
-import numpy as np
+from srcs.data_preparation.application import ApplicationPrepare
+from srcs.data_preparation.previous_application import PreviousApplicationPrepare
+from srcs.data_preparation.bureau_bureau_balance import BureauDataPrepare
+from srcs.data_preparation.credit_card_balance import CreditCardBalancePrepare
+from srcs.data_preparation.pos_cash_balance import POSCashBalancePrepare
+from srcs.data_preparation.installments_payments import InstallmentPaymentPrepare
 
-def merge_main_table(
-    application: pd.DataFrame,
-    agg_bureau: pd.DataFrame,
-    agg_prev_application: pd.DataFrame,
-    agg_cc_balance: pd.DataFrame,
-    agg_pos_cash: pd.DataFrame,
-    agg_installment_payment: pd.DataFrame
-) -> pd.DataFrame:
+class MainTableMerger:
 
-    # Merge agg_bureau
-    merge_application = application.merge(agg_bureau, on='SK_ID_CURR', how='left')
+    def __init__(self, key: str = "SK_ID_CURR"):
+        self.key = key
 
-    # Merge agg_prev_application
-    merge_application = merge_application.merge(agg_prev_application, on='SK_ID_CURR', how='left')
+    def merge(self, application: pd.DataFrame, feature_tables: list[pd.DataFrame]) -> pd.DataFrame:
 
-    # Merge agg_cc_balance
-    merge_application = merge_application.merge(agg_cc_balance, on='SK_ID_CURR', how='left')
+        main_table = application.copy()
+        for feature_table in feature_tables:
+            main_table = main_table.merge(feature_table, on=self.key, how="left")
 
-    # Merge agg_pos_cash
-    merge_application = merge_application.merge(agg_pos_cash, on='SK_ID_CURR', how='left')
+        return main_table
 
-    # Merge agg_installment_payment
-    merge_application = merge_application.merge(agg_installment_payment, on='SK_ID_CURR', how='left')
+class FeatureBuilder:
 
-    # FEATURE ENGINEERING FOR MERGE TABLE
-    # agg_prev_application
-    prev_annuity_cols = [col for col in agg_prev_application.columns if 'AMT_ANNUITY' in col]
-    for col in prev_annuity_cols:
-        merge_application['PREV_' + col + '_INCOME_TOTAL'] = merge_application[col] / (merge_application['AMT_INCOME_TOTAL'] + 1e-5)
+    def __init__(
+        self,
+        application: pd.DataFrame,
+        prev_application: pd.DataFrame,
+        bureau: pd.DataFrame,
+        bureau_balance: pd.DataFrame,
+        credit_card_balance: pd.DataFrame,
+        pos_cash_balance: pd.DataFrame,
+        instalments_payments: pd.DataFrame,
+    ):
+        self.application = application
+        self.prev_application = prev_application
+        self.bureau = bureau
+        self.bureau_balance = bureau_balance
+        self.credit_card_balance = credit_card_balance
+        self.pos_cash_balance = pos_cash_balance
+        self.instalments_payments = instalments_payments
 
-    prev_goods_cols = [col for col in agg_prev_application.columns if 'AMT_GOODS' in col]
-    for col in prev_goods_cols:
-        merge_application['PREV_' + col + '_INCOME_TOTAL'] = merge_application[col] / (merge_application['AMT_INCOME_TOTAL'] + 1e-5)
+    def agg_application(self) -> pd.DataFrame:
+        initiate = ApplicationPrepare(df=self.application)
+        return initiate.main()
 
-    # agg_cc_balance
-    cc_amt_receivable = [col for col in agg_cc_balance.columns if 'AMT_RECEIVABLE' in col]
-    for col in cc_amt_receivable:
-        merge_application['CC_BALANCE_' + col + '_AMT_INCOME_TOTAL'] = merge_application[col] / (merge_application['AMT_INCOME_TOTAL'] + 1e-5)
+    def agg_prev_application(self) -> pd.DataFrame:
+        initiate = PreviousApplicationPrepare(df=self.prev_application)
+        return initiate.main()
 
-    cc_amt_principal = [col for col in agg_cc_balance.columns if 'AMT_RECEIVABLE_PRINCIPAL' in col]
-    for col in cc_amt_principal:
-        merge_application['CC_BALANCE_' + col + '_AMT_INCOME_TOTAL'] = merge_application[col] / (merge_application['AMT_INCOME_TOTAL'] + 1e-5)
+    def agg_bureau_bureau_balance(self) -> pd.DataFrame:
+        initiate = BureauDataPrepare(bureau=self.bureau,bureau_balance=self.bureau_balance,)
+        return initiate.main()
 
-    # agg_installment_payment
-    instalment_payment_col = [col for col in agg_installment_payment.columns if 'AMT_PAYMENT' in col]
-    for col in instalment_payment_col:
-        merge_application['INST_PAYT_' + col + '_AMT_INCOME_TOTAL'] = merge_application[col] / (merge_application['AMT_INCOME_TOTAL'] + 1e-5)
+    def agg_credit_card_balance(self) -> pd.DataFrame:
+        initiate = CreditCardBalancePrepare(df=self.credit_card_balance)
+        return initiate.main()
 
-    # agg_bureau
-    bureau_days_credit_cols = [col for col in agg_bureau.columns if 'DAYS_CREDIT' in col and 'ENDDATE' not in col and 'UPDATE' not in col]
-    for col in bureau_days_credit_cols:
-        merge_application['BUREAU_' + col + '_EMPLOYED_DIFF'] = merge_application[col] - merge_application['DAYS_EMPLOYED']
-        merge_application['BUREAU_' + col + '_REGISTRATION_DIFF'] = merge_application[col] - merge_application['DAYS_REGISTRATION']
-    bureau_overdue_cols = [col for col in agg_bureau.columns if 'AMT_CREDIT' in col and 'OVERDUE' in col]
-    for col in bureau_overdue_cols:
-        merge_application['BUREAU_' + col + '_INCOME_RATIO'] = merge_application[col] /(merge_application['AMT_INCOME_TOTAL'] + 1e-5)
-    bureau_amt_annuity_cols = [col for col in agg_bureau.columns if 'AMT_ANNUITY' in col and 'CREDIT' not in col]
-    for col in bureau_amt_annuity_cols:
-        merge_application['BUREAU_' + col + '_INCOME_RATIO'] = merge_application[col] / (merge_application['AMT_INCOME_TOTAL'] + 1e-5)
+    def agg_pos_cash_balance(self) -> pd.DataFrame:
+        initiate = POSCashBalancePrepare(df=self.pos_cash_balance)
+        return initiate.main()
 
-    return merge_application
+    def agg_installments_payments(self) -> pd.DataFrame:
+        initiate = InstallmentPaymentPrepare(self.instalments_payments)
+        return initiate.main()
+
+    def build(self) -> pd.DataFrame:
+
+        agg_application = self.agg_application()
+        agg_prev_application = self.agg_prev_application()
+        agg_bureau = self.agg_bureau_bureau_balance()
+        agg_credit_card = self.agg_credit_card_balance()
+        agg_pos_cash = self.agg_pos_cash_balance()
+        agg_installments = self.agg_installments_payments()
+        merger = MainTableMerger(key="SK_ID_CURR")
+
+        main_table = merger.merge(
+            application=agg_application,
+            feature_tables=[
+                agg_bureau,
+                agg_prev_application,
+                agg_credit_card,
+                agg_pos_cash,
+                agg_installments,
+            ],
+        )
+
+        return main_table
